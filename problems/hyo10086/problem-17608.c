90c3a3bfe2679af6e983a490fd398ee1c9a39c8d
churaly <hyo10086@naver.com>
2025-06-29 23:49:30 +0900

    17608 test

---
+++ problems/hyo10086/problem-17608.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// 백준에서 문제를 푼 뒤
+// 이곳에도 코드를 복사해서
+// 깃헙에 제출하면 됨
+
+int main() {
+    int N;
+    int h_N;
+    int h_max = 0;
+    int h[100000] = {0};
+    int count = 1;
+    scanf("%d",&N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &h[i]);
+    }
+    h_N = h[N-1];
+    for (int i = N-2; i >= 0; i--)
+    {
+        if ((h[i] > h_max)&&(h[i] > h_N))
+        {
+            count++;
+            h_max = h[i];
+        }
+    }
+    printf("%d", count);
+}
